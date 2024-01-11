165d57117bac9de10e5c4147a96e2be6445bd819
itc-n23007 <n23007@std.it-college.ac.jp>
2024-01-11 07:16:58 +0000

    chapter3

--- components/header.js
+++ components/header.js
@@ -1,2 +1,13 @@
-const Header = () => <header>HEADER</header>
+import Logo from 'components/logo'
+import Nav from 'components/nav'
+import styles from 'styles/header.module.css'
+
+const Header = () => (
+  <header>
+    <div className={styles.flexContainer}>
+      <Logo boxOn />
+      <Nav />
+    </div>
+  </header>
+)
 export default Header
---
+++ components/sample.js
@@ -0,0 +1,4 @@
+const Sample = () => {
+  return <h3 style={{ color: 'green', fontSize: '2em' }}>記事タイトル</h3>
+}
+export default Sample
