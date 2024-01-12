65c1aba54a484e4b0ffbbb6cd2e507a1cb2dfde3
itc-n23007 <n23007@std.it-college.ac.jp>
2024-01-12 02:19:33 +0000

    chapter4

---
+++ components/contact.js
@@ -0,0 +1,9 @@
+import styles from 'styles/contact.module.css'
+
+const Contact = () => (
+  <div className={styles.stack}>
+    <h3 className={styles.heading}>Contact</h3>
+    <address>cube@web.maill.address</address>
+  </div>
+)
+export default Contact
---
+++ components/post-body.js
@@ -0,0 +1,6 @@
+import styles from 'styles/post-body.module.css'
+
+const PostBody = ({ children }) => (
+  <div className={styles.stack}>{children}</div>
+)
+export default PostBody
