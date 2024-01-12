5a92f04c2426ecbfe895647625526606a6e0e80c
itc-n23007 <n23007@std.it-college.ac.jp>
2024-01-12 02:21:38 +0000

    naosi

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
+import Container from 'components/container'
 import Hero from 'components/hero.js'
 
-const Home = () => {
-  return <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
-}
+const Home = () => (
+  <Container>
+    return <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
+  </Container>
+)
 export default Home
